7cd95085d7e8f632c4c9e7007d06797f33bd1bde
Fauzan Akmal Mahdi <recovery252@gmail.com>
2021-07-25 07:57:20 +0700

    update car struct

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Car struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"Desc"`
+	Content string `json:"Content"`
+}
+
+type Cars []Car
+
+func allCars(w http.ResponseWriter, r *http.Request) {
+	car := Cars{
+		Car{Title: "Test Titile", Desc: "Test Desc", Content: "Test Content"},
+	}
+	fmt.Println("Endpoint hit: All Cars Endpoint")
+	json.NewEncoder(w).Encode(car)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage Endpoint Hit")
@@ -12,6 +29,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequest() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/cars", allCars)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
